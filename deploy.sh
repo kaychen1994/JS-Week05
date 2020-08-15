de4fe17aa2e4a41ab44676220295af372609382b
kaychen1994 <kay830123@gmail.com>
2020-08-15 18:04:21 +0800

    git

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:kaychen1994/JS-Week05.github.io.git master
+git push -f git@github.com:kaychen1994/JS-Week05.github.io.git master:gh-pages
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
